60546ecb57f7e7bed87fa309e8ccbe3c480aab60
AnhNg <s3877798@rmit.edu.vn>
2022-04-16 19:32:32 +0700

    Revert "Merge branch 'thuan' into main"
    This reverts commit 966b048ebbb8d954c1ff63dbbf5c08411b5550f6.

--- CustomerCollection.java
+++ CustomerCollection.java
@@ -3,8 +3,6 @@ package GroupAssignment;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class CustomerCollection {
     CustomerTree[][][] map = new CustomerTree[26][26][26];
@@ -17,72 +15,25 @@ public class CustomerCollection {
                 }
             }
         }
-
     }
 
     public int hashC(char c) {
         return c - 'A';
     }
 
-       y.left = x;
-       x.right = sucY;
-
-        //Updating height
-        x.height = 1 + Math.max(getHeight(x.left), getHeight(x.right));
-        y.height = 1 + Math.max(getHeight(y.left), getHeight(y.right));
-        return y;
-    }
-    private int getBalance(CustomerNode node) {
-        if (node == null)
-            return 0;
-        return (getHeight(node.left) - getHeight(node.right));
-    }
-    public int hash(String id) {
-        int res = 0;
-        int n = 3;
-        for (int i = 0; i < 3; i++) {
-            char c = id.charAt(i);
-            res += (int) c * Math.pow(31, --n);
+    public boolean put(Customer newCustomer) {
+        // coordinator
+        int x = hashC(newCustomer.cusID.charAt(0));
+        int y = hashC(newCustomer.cusID.charAt(1));
+        int z = hashC(newCustomer.cusID.charAt(2));
+        if ( map[x][y][z].head == null) {
+            map[x][y][z].head = new CustomerNode(newCustomer);
         }
-        return res % MAX;
-    }
-    private CustomerNode putInTree(CustomerNode node, Customer newCustomer) {
-        if (node == null)
-            return new CustomerNode(newCustomer);
-        if (newCustomer.cusID.compareTo(node.cus.cusID) < 0)
-            node.left = putInTree(node.left, newCustomer);
-        else if (newCustomer.cusID.compareTo(node.cus.cusID) > 0)
-            node.right = putInTree(node.right, newCustomer);
         else
-            return node;
-
-       node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
-        int balance = getBalance(node);
-
-        if (balance > 1 && newCustomer.cusID.compareTo(node.left.cus.cusID) < 0) {
-            return rightRotation(node);
-        }
-        else if (balance > 1 && newCustomer.cusID.compareTo(node.left.cus.cusID) > 0) {
-            node.left = leftRotation(node.left);
-            node = rightRotation(node);
-        }
-        else if (balance < -1 && newCustomer.cusID.compareTo(node.right.cus.cusID) > 0) {
-            return leftRotation(node);
-        }
-        else if (balance < -1 && newCustomer.cusID.compareTo(node.right.cus.cusID) < 0) {
-           node.right = rightRotation(node.right);
-           return leftRotation(node);
-        }
-        return node;
+            map[x][y][z].head = map[x][y][z].putInTree(map[x][y][z].head, newCustomer);
+        return true;
     }
-    public void put(Customer newCustomer) {
-        int idx = hash(newCustomer.cusID);
-        if (table[idx] == null) {
-            table[idx] = new CustomerNode(newCustomer);
-            return;
-        }
-        table[idx] = putInTree(table[idx], newCustomer);
-    }
+
     public void readFile(String fileName) {
         BufferedReader reader = null;
         try {
@@ -112,51 +63,11 @@ public class CustomerCollection {
             }
         }
     }
-    private CustomerNode searchInTree(CustomerNode node, String id) {
-        if (node == null || id.equals(node.cus.cusID))
-            return node;
-        if (id.compareTo(node.cus.cusID) < 0)
-            return searchInTree(node.left, id);
-        else
-            return searchInTree(node.right, id);
-    }
     public CustomerNode search(String id) {
-        int idx = hash(id);
-        if (table[idx].cus.cusID.equals(id))
-            return table[idx];
-        else
-            return searchInTree(table[idx], id);
-    }
-
-    private void printTree(CustomerNode node) {
-        if (node == null)
-            return;
-        printTree(node.left);
-        System.out.println(node.cus.fName);
-        printTree(node.right);
-    }
-
-    public void print() {
-        for (CustomerNode x : table) {
-            if (x != null)
-                printTree(x);
-        }
-    }
-
-    public void searchPartial(String id) {
-        int idx = hash(id);
-        Queue<CustomerNode> queue = new LinkedList<>();
-        queue.add(table[idx]);
-        while (!queue.isEmpty()) {
-            CustomerNode root = queue.remove();
-            if(root.cus.cusID.contains(id)) {
-                System.out.println(root.cus.cusID + " " + root.cus.fName);
-            }
-            if (root.left != null)
-                queue.add(root.left);
-            if (root.right != null)
-                queue.add(root.right);
-        }
+        int x = hashC(id.charAt(0));
+        int y = hashC(id.charAt(1));
+        int z = hashC(id.charAt(2));
+        return map[x][y][z].searchInTree(map[x][y][z].head, id);
     }
 
     public static void main(String[] args) {
@@ -164,9 +75,10 @@ public class CustomerCollection {
         CustomerCollection col = new CustomerCollection();
         long start = System.currentTimeMillis();
         col.readFile(myFile);
-        long start = System.currentTimeMillis();
-        col.searchPartial("NAO1931");
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
+        System.out.println(col.search("NAO1931262").getCus().fName);
+//        System.out.println(col.search("NAO1931162").getCus().fName);
+//        System.out.println(col.search("NAO1933232").getCus().fName);
+//        System.out.println(col.search("NAO1421234").getCus().fName);
+//        System.out.println(col.search("NAO1423234").getCus().fName);
     }
 }
